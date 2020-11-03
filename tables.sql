f5b041fe956c696ef18f1034c21844844e0a04d8
louislaugier <l_laugier@etu-webschoolfactory.fr>
2020-11-03 01:13:13 +0100

    update

--- tables.sql
+++ tables.sql
@@ -1,7 +1,5 @@
--- Sequence and defined type
 CREATE SEQUENCE IF NOT EXISTS questions_id_seq;
 
--- Table Definition
 CREATE TABLE "public"."questions" (
     "id" int4 NOT NULL DEFAULT nextval('questions_id_seq'::regclass),
     "question" text NOT NULL,
@@ -12,14 +10,7 @@ CREATE TABLE "public"."questions" (
     PRIMARY KEY ("id")
 );
 
--- Sequence and defined type
-CREATE SEQUENCE IF NOT EXISTS scores_id_seq;
-
--- Table Definition
 CREATE TABLE "public"."scores" (
-    "id" int4 NOT NULL DEFAULT nextval('scores_id_seq'::regclass),
     "username" varchar NOT NULL,
-    "score" int2 NOT NULL,
-    PRIMARY KEY ("id")
+    "score" int2 NOT NULL
 );
-
