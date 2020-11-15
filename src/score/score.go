8a6a2da8a1546344248ac08a4d85617a503cb41a
louislaugier <l_laugier@etu-webschoolfactory.fr>
2020-11-15 18:52:52 +0100

    update

--- src/score/score.go
+++ src/score/score.go
@@ -17,6 +17,7 @@ type score struct {
 
 // POST new score
 func POST(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	score := &score{}
 	json.NewDecoder(r.Body).Decode(&score)
